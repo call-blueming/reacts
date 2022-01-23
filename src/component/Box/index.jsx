012a91c5f0c7a1a8492a45e95363425374c6ba7f
王先生 <7538229+thishoter@user.noreply.gitee.com>
2022-01-23 22:03:23 +0800

    修改文件-react

---
+++ src/component/Box/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import './index.css'
+class Box extends Component {
+    render() {
+        return (
+            <div className='box'>大盒子</div>
+
+        )
+    }
+}
+export default Box
---
+++ src/component/Hello/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import './index.css'
+class Hello extends Component {
+    render() {
+        return (
+            <div className='hello'>大盒子</div>
+
+        )
+    }
+}
+export default Hello
